88abcf25640b26073592f2ad8192cf1b1ae239d2
tlubowiecki <t.lubowiecki@nordwerft.de>
2021-05-21 11:43:57 +0200

    Update

--- Playground/src/de/lubowiecki/javakurs/InterfaceTest6.java
+++ Playground/src/de/lubowiecki/javakurs/InterfaceTest6.java
@@ -1,5 +1,11 @@
 package de.lubowiecki.javakurs;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+
 public class InterfaceTest6 {
 
 	public static void main(String[] args) {
@@ -15,8 +21,41 @@ public class InterfaceTest6 {
 		Az1 a1 = new Az1();
 		//Az2 a2 = (Az2) a1; // Mit Casting zum Klassen-Typ ist es NICHT kompilierbar
 		
-		int x;
-		//System.out.println(x); //
+		System.out.println();
+		
+		float d1 = 0.3f;
+		float d2 = 0.0f;
+		
+		System.out.println(d1/d2);
+		
+		//Az1 az = (Az1)(new Object());
+		
+		String str;
+		boolean b = false;
+		
+		if(b = true)
+			str = "true";
+		
+		//str = (b) ? str : "false";
+		//System.out.println(str);
+		
+//		Int hat 32bit, das sind 4 Bytes
+//		00000000 + 00000000 + 00000000 + 00000000
+		
+		Period p = Period.of(1, 1, 0);
+		System.out.println(p.getDays());
+		System.out.println(p.getMonths());
+		
+		DateTimeFormatter f = DateTimeFormatter.ofPattern("yyyy/M/d");
+		LocalDate ld = LocalDate.parse("2000/11/5", f);
+		System.out.println(ld); //2000-01-05
+		System.out.println(ld.format(f));
+		
+		//ChronoUnit.DAYS
+		System.out.println(ChronoField.valueOf("MINUTE_OF_DAY"));
+		ld.getLong(ChronoField.valueOf("MINUTE_OF_DAY"));
+		
+		
 		
 	}
 }
