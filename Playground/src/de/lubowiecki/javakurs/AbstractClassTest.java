4bd910af8886a22fc4ba544cfeb2248ea61b3aab
tlubowiecki <t.lubowiecki@nordwerft.de>
2021-05-25 14:47:39 +0200

    Update

--- Playground/src/de/lubowiecki/javakurs/AbstractClassTest.java
+++ Playground/src/de/lubowiecki/javakurs/AbstractClassTest.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 
 public class AbstractClassTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 
 		// AbstractPerson ap = new AbstractPerson();
 		
