25865b78ae8ebabed7455d7131ffc1755f993cb4
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2021-07-08 14:09:11 +0200

    HTML & Co

---
+++ GameServer/src/main/java/de/lubowiecki/game/mq/ServerMQ.java
@@ -0,0 +1,27 @@
+package de.lubowiecki.game.mq;
+
+import org.zeromq.SocketType;
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
+
+public class ServerMQ {
+	
+	public static void main(String[] args) throws Exception
+    {
+		try (ZContext context = new ZContext()) {
+            // Socket to talk to clients
+            ZMQ.Socket socket = context.createSocket(SocketType.REP);
+            socket.bind("tcp://*:5000");
+
+            while (!Thread.currentThread().isInterrupted()) {
+                byte[] reply = socket.recv(0);
+                System.out.println("Received " + ": [" + new String(reply, ZMQ.CHARSET) + "]");
+
+                Thread.sleep(1000); //  Do some 'work'
+
+                String response = "world";
+                socket.send(response.getBytes(ZMQ.CHARSET), 0);
+            }
+        }
+    }
+}
