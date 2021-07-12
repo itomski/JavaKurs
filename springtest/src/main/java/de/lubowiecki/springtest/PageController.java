a8ea6c97f6dd3d925b2128aebfa703cf34c56803
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2021-07-12 14:57:37 +0200

    Update

--- springtest/src/main/java/de/lubowiecki/springtest/PageController.java
+++ springtest/src/main/java/de/lubowiecki/springtest/PageController.java
@@ -1,14 +1,56 @@
 package de.lubowiecki.springtest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import de.lubowiecki.springtest.model.Product;
 
 @Controller
 public class PageController {
 	
 	// URL unter der dise Methode verfügbar ist: http://localhost:8080/test
-	@RequestMapping("/test")
-	public String machWas() {
+	@RequestMapping("/")
+	public String homeAction(Model model) { // model = Template-Objekt
+		model.addAttribute("title", "Startseite");
+		model.addAttribute("nav", "home");
+		return "standard"; // Name des Templates
+	}
+	
+	@RequestMapping("/products")
+	public String productsAction(Model model) {
+		model.addAttribute("title", "Unsere Produkte");
+		model.addAttribute("nav", "products");
+		
+		List<Product> produkte = new ArrayList<>();
+		produkte.add(new Product("Super BUHA", "Top Buchhaltungs Software", 100.99));
+		produkte.add(new Product("TopTimer", "Software für die Zeiterfassung", 29.99));
+		produkte.add(new Product("RechnungFix", "Schnell Rechnungen erstellen", 29.99));
+		
+		model.addAttribute("produkte", produkte);
+		return "standard"; // Name des Templates
+	}
+	
+	@RequestMapping("/services")
+	public String serviceAction(Model model) {
+		model.addAttribute("title", "Unsere Services");
+		model.addAttribute("nav", "service");
+		return "standard"; // Name des Templates
+	}
+	
+	@RequestMapping("/team")
+	public String teamAction(Model model) {
+		model.addAttribute("title", "Das Team");
+		model.addAttribute("nav", "team");
+		return "standard"; // Name des Templates
+	}
+	
+	@RequestMapping("/contact")
+	public String contactAction(Model model) {
+		model.addAttribute("title", "Schreiben Sie uns");
+		model.addAttribute("nav", "contact");
 		return "standard"; // Name des Templates
 	}
 }
---
+++ springtest/src/main/java/de/lubowiecki/springtest/model/Product.java
@@ -0,0 +1,41 @@
+package de.lubowiecki.springtest.model;
+
+import java.io.Serializable;
+
+// POJO - Plain Old Java Object
+public class Product implements Serializable {
+
+	private static final long serialVersionUID = 1239869214818948111L;
+	
+	private String name;
+	private String description;
+	private double price;
+	
+	public Product() {
+	}
+	
+	public Product(String name, String description, double price) {
+		this.name = name;
+		this.description = description;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+}
